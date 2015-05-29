4796d3118eeb672b466bbd6b7eb20974b9a9fcf2
Andy <andyv.42@gmail.com>
2015-05-28 22:35:47 -0700

    Lin alg file

---
+++ src/LinearAlgebra.java
@@ -0,0 +1,40 @@
+/**
+ * @author Andy Vuong
+ */
+
+public class LinearAlgebra {
+
+	/**
+	 * Decomposes a (n x n) dimensional JpArray into a lower and upper triangular matrix.
+	 * 
+	 * @param A	Matrix to decompose.
+	 * @return L,U Matrices stored in a JpArray object.
+	 */
+	public static JpArray[] lu(JpArray A) {
+		
+		return null;
+	}
+	
+	/**
+	 * Decomposes a (n x n) dimensional JpArray into Q and R, orthogonal and upper triangular matrices.
+	 * 
+	 * @param A	Matrix to decompose.
+	 * @return Q,R Matrices stored in a JpArray object.
+	 */
+	public static JpArray[] qr(JpArray A) {
+		
+		return null;
+	}
+	
+	/**
+	 * Decomposes a (n x n) dimensional JpArray into matrices containing the left singular vectors, singular values, and right singular vectors.
+	 * 
+	 * @param A	Matrix to decompose.
+	 * @return u, sigma, vT Matrices stored in a JpArray object.
+	 */
+	public static JpArray[] svd(JpArray A) {
+		
+		return null;
+	}
+	
+}
