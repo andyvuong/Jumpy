63efbd306af0af5279d994c601ca10a98da6bd4e
Andy <andyv.42@gmail.com>
2015-05-30 20:37:59 -0700

    inner product

--- src/LinearAlgebra.java
+++ src/LinearAlgebra.java
@@ -4,8 +4,6 @@
 
 public class LinearAlgebra {
 
-
-	
 	/**
 	 * Take the inner product of a (m x n) and (p x q) JpArrays. In the case of vector multiplication (1-D JpArrays), a scalar is returned as a JpArray of one index.
 	 *
@@ -16,14 +14,32 @@ public class LinearAlgebra {
 	public static JpArray dot(JpArray A, JpArray B) {
 		int[] ADim = A.getShape();
 		int[] BDim = B.getShape();
-		if(ADim.length == 1 || BDim.length == 1 && ADim[0] == BDim[0]) {
+		if(ADim.length > 2 || BDim.length > 2) {
+			throw new IllegalArgumentException("This method currently cannot take the inner product of JpArrays with dimensions greater than 2!");
+		}
+		if(ADim.length == 1 && BDim.length == 1 && ADim[0] == BDim[0]) {
 			JpArray newJp = new JpArray(0.0, ADim);
 			for(int i=0; i<ADim[0]; i++) {
 				newJp.setValue((Double) A.getValue(i) * (Double) B.getValue(i), i);
 			}
 			return newJp;
 		}
-		
+		else if(ADim.length == 1 && BDim.length > 1) {
+			JpArray temp = A.transpose();
+			if(ADim[0] == BDim[0]) {
+				int[] newDim = {1, BDim[1]};
+				JpArray newJp = new JpArray(0.0, newDim);
+				for(int i=0; i<newDim.length; i++) {
+					for(int j=0; j<newDim[i]; j++) {
+						
+					}
+				}
+				return newJp;
+			}			
+		}
+		else {
+			throw new IllegalArgumentException("JpArray dimension mismatch!");
+		}
 		return null;
 	}
 	
